7ab75c845587bbd4d6058a152cf56f328369e0af
Nandini shukla <78168737+OkayMini@users.noreply.github.com>
2023-02-27 16:28:01 +0530

    Project_commit

---
+++ Assignment _Project/Portfolio.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+from matplotlib import image
+
+PAGE_TITLE = "Nandini Shukla|Portfolio"
+PAGE_ICON = ":wave:"
+
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON,initial_sidebar_state="auto")
+
+
+st.title(":blue[Nandini shukla] :wave:")
+
+#st.title("Dashboard - Iris Data")
+
+st.image('Profile.png',width=250)
+
+st. header('My Portfolio :sunglasses:')
+#st.write('Here is a breakdown of my portfolio:')
+st.write("A data science enthusiast with experience in Python language, have a strong foundation in data analysis, visualization, and machine learning. I'm also comfortable working with various types of data sources, building and evaluating machine learning models, and using data visualization techniques to communicate insights effectively.")
+
+
+st.subheader("Skills & Tools ⚒️")
+
+btn_click = st.button("TOOLS")
+
+if btn_click == True:
+    st.write(""" Jupyter Notebook ,
+             VSCode, Python Web Scraping Tools,
+             Requests,
+             Beautiful Soup.
+             """)
+    st.balloons()
+
+btn_click = st.button("SKILLS")
+
+if btn_click == True:
+    st.write("Python, Numpy,Pandas, Data Analysis")
+    st.balloons()
+
+
+st.subheader("Contect me :coffee:")
+
+st.markdown("Github Account Link - https://github.com/OkayMini")
+
+st.markdown("Linkedin Account Link - https://www.linkedin.com/in/nandini-shukla-40ab9020a/")
+
---
+++ Assignment _Project/pages/World_Happiness_Dashboard.py
@@ -0,0 +1,70 @@
+import streamlit as st
+from matplotlib import image
+import pandas as pd
+import plotly.express as px
+import os
+
+# absolute path to this file
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# absolute path to this file's root directory
+PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+# absolute path of directory_of_interest
+dir_of_interest = os.path.join(PARENT_DIR, "resources")
+
+#IMAGE_PATH = os.path.join(dir_of_interest, "images", "Happiness.jpg")
+
+DATA_PATH = os.path.join(dir_of_interest, "data", "New.csv")
+
+st.write('''
+# :red[INNOMATICS] Intership Assignment:writing_hand: 
+''')
+st.write('''
+##### Hello everyone, I've created a simple VIsualization Dashboard on World Happiness Report.
+##### 
+''')
+
+st.subheader("Dashboard - World Happiness Report 2021")
+
+st.write('''
+## _Dataset_ 
+#### *World Happines Report*
+''')
+
+df = pd.read_csv(DATA_PATH)
+st.dataframe(df)
+
+
+st.write('''
+## _Dataset_ 
+#### *World Happines Report*
+''')
+
+st.write('''
+###### This dasboard shows the many depedencies in World happines.
+###### Below is the histogarm and a box plot which repesent the Freedom to make life choices by the number of people in a country
+''')
+
+Country = st.selectbox("Select the Country:", df['Country name'].unique())
+
+col1, col2 = st.columns(2)
+
+fig_1 = px.histogram(df[df['Country name'] == Country], x="Freedom to make life choices")
+col1.plotly_chart(fig_1, use_container_width=True)
+
+fig_2 = px.box(df[df['Country name'] == Country], x="Freedom to make life choices")
+col2.plotly_chart(fig_2, use_container_width=True)
+
+
+st.write('''
+
+###### Above is the histogarm and a box plot which repesent the rate ofHealthy life expectancy at birth by the number of people in a country
+''')
+
+fig_1 = px.histogram(df[df['Country name'] == Country], x="Healthy life expectancy at birth")
+col1.plotly_chart(fig_1, use_container_width=True)
+
+fig_2 = px.box(df[df['Country name'] == Country], x="Healthy life expectancy at birth")
+col2.plotly_chart(fig_2, use_container_width=True)
+
+
+st.line_chart(data=df,x = 'Country name',y=['Positive affect','Negative affect'])
